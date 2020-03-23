8ff6bcbe65a6d4c0478395c58848533b0c343f02
Suddha Mukhopadhyay <suddha@bitcoin.com.au>
2020-03-23 08:21:08 +0530

    Add vue resource Add firebase post/get

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,106 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div class="columns">
+      <div class="colmun">
+        <div class="card">
+          <header class="card-header">
+            <p class="card-header-title">
+              Enter your details
+            </p>
+          </header>
+          <div class="card-content">
+            <div class="content">
+              <div class="field is-horizontal">
+                <div class="field-label is-normal">
+                  <label class="label">Username</label>
+                </div>
+                <div class="field-body">
+                  <div class="field">
+                    <p class="control">
+                      <input
+                        v-model="user.username"
+                        class="input"
+                        type="text"
+                        placeholder="Enter an username"
+                      />
+                    </p>
+                  </div>
+                </div>
+              </div>
+
+              <div class="field is-horizontal">
+                <div class="field-label is-normal">
+                  <label class="label">Email</label>
+                </div>
+                <div class="field-body">
+                  <div class="field">
+                    <p class="control">
+                      <input
+                        v-model="user.email"
+                        class="input"
+                        type="email"
+                        placeholder="Enter an email"
+                      />
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <footer class="card-footer">
+            <button class="button is-primary" @click.prevent="submit">
+              Submit
+            </button>
+            <button class="button is-success" @click.prevent="getData">
+              Get Data
+            </button>
+            Delete
+          </footer>
+        </div>
+      </div>
+    </div>
+
+    <div class="columns">
+      <div class="column">
+        <ul class="list">
+          <li class="list-item" v-for="(user, key) in fireStoreData" :key="key">
+            Username: {{ user.username }}
+
+            <br />
+
+            Email: {{ user.email }}
+          </li>
+        </ul>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
 export default {
   name: "App",
-  components: {
-    HelloWorld
+  data() {
+    return {
+      user: {
+        username: "",
+        email: ""
+      },
+      fireStoreData: []
+    };
+  },
+  methods: {
+    submit() {
+      this.$http
+        .post("", this.user)
+        .then(response => console.log(response))
+        .catch(error => console.error(error));
+    },
+    getData() {
+      this.$http
+        .get('')
+        .then(({ data }) => (this.fireStoreData = data))
+        .catch(error => console.error(error));
+    }
   }
 };
 </script>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import Vue from "vue";
 import App from "./App.vue";
+import VueResource from "vue-resource";
 
 Vue.config.productionTip = false;
-
+Vue.use(VueResource);
+Vue.http.options.root =
+  "https://vue-resource-udemy-1f7ee.firebaseio.com/data.json";
 new Vue({
   render: h => h(App)
 }).$mount("#app");
